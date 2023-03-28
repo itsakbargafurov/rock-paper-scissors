16def1b896ab5cb29912c797e0defae72d0f25d6
akbar <akbgafurov@gmail.com>
2023-03-28 10:22:13 +0500

    Change console.log in playRound function to return

--- javascript.js
+++ javascript.js
@@ -26,27 +26,27 @@ function playRound (player, computer) {
     // Play the game
     if (player == "rock") {
         if (computer == "paper") {
-            console.log("you lose! Paper beats Rock.");
+            return "you lose! Paper beats Rock.";
         } else if (computer == "scissors") {
-            console.log("you win! Rock beats Scissors.");
+            return "you win! Rock beats Scissors.";
         } else {
-            console.log("it's a tie! Rock ties Rock.");
+            return "it's a tie! Rock ties Rock.";
         }
     } else if (player == "paper") {
         if (computer == "rock") {
-            console.log("you win! Paper beats Rock.");
+            return "you win! Paper beats Rock.";
         } else if (computer == "scissors") {
-            console.log("you lose. Scissors beat Paper.");
+            return "you lose. Scissors beat Paper.";
         } else {
-            console.log("it's a tie! Paper ties Paper.");
+            return "it's a tie! Paper ties Paper.";
         }
     } else {
         if (computer == "paper") {
-            console.log("you win! Scissors beat Paper.");
+            return "you win! Scissors beat Paper.";
         } else if (computer == "rock") {
-            console.log("you lose! Rock beats Scissors.");
+            return "you lose! Rock beats Scissors.";
         } else {
-            console.log("it's a tie. Scissors tie Scissors")
+            return "it's a tie. Scissors tie Scissors";
         }
     }
 }
